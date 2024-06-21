05d91f56e688296ef5267fa2f27a75a0c0b6efb6
KiranJohns <mekiranjohnson@gmail.com>
2024-06-21 15:48:23 +0530

    take screenshot

--- App.js
+++ App.js
@@ -7,64 +7,79 @@ import * as ImagePicker from 'expo-image-picker';
 import { useState } from 'react';
 import CircleButton from './components/CircleButton';
 import IconButton from './components/IconButton';
+import EmojiPicker from "./components/EmojiPicker";
+import EmojiList from './components/EmojiList';
+import EmojiSticker from './components/EmojiSticker';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const Beach = require('./assets/background-image.png')
 
 export default function App() {
   const [showAppOptions, setShowAppOptions] = useState(false);
-  const [selectedImage, setSelectedImage] = useState(null)
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [pickedEmoji, setPickedEmoji] = useState(null);
 
-  const pickImageAsync = async () =>{
+
+  const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
       quality: 1,
     });
-       if(!result.canceled){
-        setSelectedImage(result.assets[0].uri)
-        setShowAppOptions(true);
-       } else{
-        alert('You did not select any image')
-       }
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri)
+      setShowAppOptions(true);
+    } else {
+      alert('You did not select any image')
+    }
   }
+
+
 
   const onReset = () => {
     setShowAppOptions(false);
   };
 
   const onAddSticker = () => {
-    // we will implement this later
+    setIsModalVisible(true);
   };
 
   const onSaveImageAsync = async () => {
     // we will implement this later
   };
 
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer 
-        placeholderImageSource={Beach} 
-        selectedImage={selectedImage}
+        <ImageViewer
+          placeholderImageSource={Beach}
+          selectedImage={selectedImage}
         />
-        <StatusBar style="auto" />
+       {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
       </View>
       {showAppOptions ? (
-         <View style={styles.optionsContainer}>
-         <View style={styles.optionsRow}>
-           <IconButton icon="refresh" label="Reset" onPress={onReset} />
-           <CircleButton onPress={onAddSticker} />
-           <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
-         </View>
-       </View>
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+          </View>
+        </View>
       ) : (
-      <View style={styles.footerContainer}>
-         <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
-         <Button label="Use this photo"  onPress={() => setShowAppOptions(true)} />
-      </View>
-        )}
+        <View style={styles.footerContainer}>
+          <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+          <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
+        </View>
+      )}
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+      <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+      </EmojiPicker>
       <StatusBar style="auto" />
-    </View>
+    </GestureHandlerRootView>
   );
 
 }
@@ -89,7 +104,7 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 58,
   },
-    optionsContainer: {
+  optionsContainer: {
     position: 'absolute',
     bottom: 80,
   },
@@ -97,4 +112,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flexDirection: 'row',
   },
+ 
 });
+
